2fe102e231f07bf5fee9d0f55f29ce011390fa4d
pennyc523 <pennyc523@gmail.com>
2017-12-03 21:15:35 -0500

    add annotation

--- simulate.m
+++ simulate.m
@@ -1,16 +1,11 @@
 function [X1, X2, T, C, dN,m,theta]=simulate(n,beta0,beta1,tau,v1,v2)
 
-X1= unifrnd(0,1,n,1); X2=repmat(binornd(1,0.5),[n,1]); %X2 is the I(monozygotic twins indicator)
-
-if X2== repmat(1,[n,1]) 
-    theta=gamrnd(1/v1,v1);
-else
-    theta=gamrnd(1/v2,v2);
-end
- 
-%in the same cluster, the theta follows the same frailty distribution
+X1= unifrnd(0,1,n,1); X2=repmat(binornd(1,0.5),[n,1]); 
+%X1 is uniform(0,1), X2 is bernoulli (0,0.5)
 C=min(unifrnd(2.5,8,n,1),repmat(tau, n, 1));  
-[T,dN,m]=poisson(C, X1, X2, beta0,beta1,theta,tau);
-
+%C is the censoring time which is the 
+[T,dN,m]=poisson(C, X1, X2, beta0,beta1,tau);
+% [T,dN,m] is output of poisson function, 
+%where  T=observed event time, dN=counting increment (either 0 or 1) , m=total number of events observed]
 
 
