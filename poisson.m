8e5d179f5b9b9194cdf9e4a0fc32753ef309aa11
pennyc523 <pennyc523@gmail.com>
2017-12-03 21:11:16 -0500

    update poisson.m
    This is not a random effect generating model.

--- poisson.m
+++ poisson.m
@@ -1,33 +1,43 @@
-function   [obsT,dN,m]=poisson(C, X1, X2, beta0,beta1,theta,tau)
+function   [obsT,dN,m]=poisson(C, X1, X2, beta0,beta1,tau)
+%this function needs to input censoring time, X1, X2, beta0, beta1, tau 
+% output of this function is realization of n poisson process. 
 
+
+%C is censoring time
+%X1 is a random number ~ Bernoulli(0,p)
+%X2 is a random numver ~ Uniform (0,1)
+%tau is the maxium observational time
 [n, k]=size(C);
 
 obst = zeros(n, 100);
 
 m= zeros(n, 1);
-% SI = unifrnd(1,3.5,n,1) %editting
+
 
 
  
 for i=1:n
-    S = zeros(1, 100);
- lmt = exp(theta*(abs(beta0)  + abs(beta1))*theta) ; mn=0;t=-log(rand) / lmt;
+    S = zeros(1, 100);  % maximum event occurrence of a single realization is 100. 
+ lmt = exp(abs(beta0)  + abs(beta1)) ; 
+ %lmt is the constant upper bound of the nonhomogenous poisson process rate lambda=exp(beta0*x1+beta1*x2)
+ %which you want to generate data from
+ mn=0;
+ t=-log(rand) / lmt;
+ % thinning method, initalize the event time as -log(rand)/lmt, where rand is a random number from Uniform(0,1)
 
-    while t < tau
-            if rand < exp(theta*(beta0 * X1(i) + beta1*X2(i))) / lmt, mn=mn+1; S(mn)=t; end
-
+    while t < tau  
+    % when event time < tau ,where observation period is from 0 to tau
+            if rand < exp((beta0 * X1(i) + beta1*X2(i)) / lmt, mn=mn+1; 
+            S(mn)=t; 
+            end
+     % by accepting-rejection critiera, if an independent random number from Uniform(0,1) is smaller than lambda/lmt, 
+     %then update the event time as t-log(rand)/lmt; else not update
             t=t-log(rand) / lmt;
     end
 
-%lmt2 = exp( 0.2  + beta* X2(i) + 0.5* X3(i) ); t= SI(i)-log(rand) / lmt2;
-
-%     while t<tau
-%                if rand < exp( 0.2*cos(t)* X1(i)+ beta* X2(i) + 0.5*sin(t-SI(i)) * X3(i) )/ lmt2, mn=mn+1; S(mn)=t; end
-
-%    t=t-log(rand) / lmt2;
-%    end
 
 obst(i, 1:mn)  = sort(S(1:mn), 1) .* (sort(S(1:mn),1) <=C(i));
+%sort the observed time, only keep those smaller than the censoring time 
 end
 
 dN=double(obst>0);
