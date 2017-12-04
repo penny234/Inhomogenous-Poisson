3fec20c9fb6d7d78c80d427aab3e8e4c400abd45
pennyc523 <pennyc523@gmail.com>
2017-12-03 21:20:27 -0500

    Update simulate.m

--- simulate.m
+++ simulate.m
@@ -2,7 +2,7 @@ function [X1, X2, T, C, dN,m,theta]=simulate(n,beta0,beta1,tau)
 
 X1= unifrnd(0,1,n,1); X2=repmat(binornd(1,0.5),[n,1]); 
 %X1 is uniform(0,1), X2 is bernoulli (0,0.5)
-C=min(unifrnd(2.5,8,n,1),repmat(tau, n, 1));  
+C=rand(0,tau);  
 %C is the censoring time which is the 
 [T,dN,m]=poisson(C, X1, X2, beta0,beta1,tau);
 % [T,dN,m] is output of poisson function, 
