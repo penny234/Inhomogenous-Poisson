bd34420357a0f5d2f2e2ef7e461c7165f5437873
pennyc523 <pennyc523@gmail.com>
2017-12-03 21:24:00 -0500

    Update main2.m

--- main2.m
+++ main2.m
@@ -1,19 +1,16 @@
 clear all
 tic
 
+N=1000;  % do 1000 simulations
+i=1;
 
-i=0;
-
-while i<100
+while i<=N
        [A,T]=f1();
-       dlmwrite(sprintf('time%d.txt', i), T,'delimiter','\t','precision','%.6f');
+       dlmwrite(sprintf('time%d.txt', i), T,'delimiter','\t','precision','%.6f'); 
+       % record the results as time1.txt, time2.txt,...., time1000,  in your current path 
+       % record the results as A1.txt, A2.txt,...., A1000.txt,  in your current path 
        dlmwrite(sprintf('A%d.txt', i), A,'delimiter','\t','precision','%.6f');
        i=i+1;
        
 end
 
-
-    
-
-%dlmwrite('/Users/Penny/Desktop/time.txt',T,'delimiter','\t','precision','%.6f')
-%dlmwrite('/Users/Penny/Desktop/test.txt',A,'delimiter','\t','precision','%.6f')
